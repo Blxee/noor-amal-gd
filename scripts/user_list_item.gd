f772d0009541b1da727a322a3d166b26320030db
Blxee <aminedev57@gmail.com>
2024-09-20 18:21:23 +0100

    Added: payment arrows

--- scripts/user_list_item.gd
+++ scripts/user_list_item.gd
@@ -2,6 +2,7 @@ extends Panel
 
 const CANCEL = preload("res://assets/cancel.svg")
 const CHECK = preload("res://assets/check.svg")
+const CHEVRON = preload("res://assets/chevron.svg")
 
 @onready var entry: Entry:
 	set(value):
@@ -9,6 +10,23 @@ const CHECK = preload("res://assets/check.svg")
 		%FirstName.text = entry.first_name
 		%LastName.text = entry.last_name
 		%Assurance.texture = CHECK if entry.assurance else CANCEL
+		
+		var months_due: int = roundi(Time.get_date_dict_from_unix_time(Time.get_unix_time_from_system() - entry.last_payment).get('month'))
+		if months_due > 0:
+			%Payment.modulate = Color('#f52c58')
+			%Payment/Month.show()
+			%Payment/Month.text = str(abs(months_due))
+			%Payment/Texture.texture = CHEVRON
+			%Payment/Texture.flip_v = true
+		elif months_due < 0:
+			%Payment.modulate = Color('#45d918')
+			%Payment/Month.show()
+			%Payment/Month.text = str(abs(months_due))
+			%Payment/Texture.texture = CHEVRON
+			%Payment/Texture.flip_v = false
+		elif months_due == 0:
+			%Payment.modulate = Color('#45d918')
+			%Payment/Month.hide()
 
 
 func _on_gui_input(event: InputEvent) -> void:
